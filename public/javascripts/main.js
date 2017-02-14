e08b12d2671302fefb04bc4c6f18a93b10ebcf1d
nicolassims <sableye.nico@gmail.com>
2017-02-14 14:49:47 -0500

    Working on serviceWorkers

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,16 +15,9 @@ class main {
 }
 
 class serviceWorkers {
-    static setServiceWorker() {
+    static loadServiceWorker() {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker
-                .register('./service-worker.js', {scope:'./'})
-                .then(function(registration) {
-                    console.log("Service Worker Registered", registration);
-            })
-            .catch(function(err) {
-                console.log("Service worker failed to register", err);
-            })
+            navigator.serviceWorker.register('/ServiceWorker.js');
         }
     }
 }
