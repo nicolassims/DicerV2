23d663020418ae5820b421caa88ee6d5d23a90d6
nicolassims <sableye.nico@gmail.com>
2017-02-17 13:53:05 -0500

    Small color changes

--- service-worker.js
+++ service-worker.js
@@ -34,11 +34,3 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
-});
